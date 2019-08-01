366f4827db74c4eaa3fa80532405318ab514aa52
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-01 14:46:48 +0500

    --Commit: QR rendered using the serialized JSON

--- lib/src/widget/QRCodeImage.dart
+++ lib/src/widget/QRCodeImage.dart
@@ -2,12 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
 class QRCodeImage extends StatelessWidget {
+  final String product;
+
+  const QRCodeImage({Key key, this.product}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 60.0),
       child: QrImage(
-        data: 'kajbnds',
+        data: product,
         size: 200,
       ),
     );
