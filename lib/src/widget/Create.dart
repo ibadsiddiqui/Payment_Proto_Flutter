b3dd1668c44ccc01a6447bd677dc53c6b942b3c2
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-01 14:38:02 +0500

    --Commit: Conditional rendering used for rendering QRCode, if Product ID invalid.

--- lib/src/widget/Create.dart
+++ lib/src/widget/Create.dart
@@ -8,10 +8,23 @@ class Create extends StatefulWidget {
 }
 
 class _CreateState extends State<Create> {
+
+  String _product;
+  void _productCallBack(product){
+    setState(() {
+      _product = product;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    if(_product !=null)
     return Column(
-      children: <Widget>[QRForm(), QRCodeImage()],
+      children: <Widget>[QRForm(serializedCallback: _productCallBack,), QRCodeImage(product: _product,)],
     );
+    else return Column(
+      children: <Widget>[QRForm(serializedCallback: _productCallBack,),Container()],
+    );
+
   }
 }
